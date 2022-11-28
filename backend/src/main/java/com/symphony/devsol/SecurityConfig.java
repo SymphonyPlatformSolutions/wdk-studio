65a4a34a4f7c6e6eb56df64c59ff4bec2b1c1bcf
Yong Sheng Tan <yong.tan@symphony.com>
2022-11-28 13:35:38 +0800

    Disabled CORS

--- backend/src/main/java/com/symphony/devsol/SecurityConfig.java
+++ backend/src/main/java/com/symphony/devsol/SecurityConfig.java
@@ -1,31 +1,13 @@
 package com.symphony.devsol;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.List;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().headers().frameOptions().disable();
-    }
-
-    @Profile("dev")
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        http.cors().disable().csrf().disable().headers().frameOptions().disable();
     }
 }
