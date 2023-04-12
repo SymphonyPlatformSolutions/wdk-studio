da702a5c90ffc3fd53b470945f017c1a836995b1
Yong Sheng Tan <yong.tan@symphony.com>
2023-04-12 11:51:06 +0800

    Revised Dockerfile to use WDK base image

--- src/main/java/com/symphony/devsol/client/SymphonyClient.java
+++ src/main/java/com/symphony/devsol/client/SymphonyClient.java
@@ -4,6 +4,8 @@ import com.symphony.bdk.core.auth.jwt.UserClaim;
 import com.symphony.bdk.core.service.user.UserService;
 import com.symphony.bdk.gen.api.model.UserSearchQuery;
 import com.symphony.bdk.gen.api.model.UserV2;
+import com.symphony.devsol.model.wdk.Profile;
+import com.symphony.devsol.model.wdk.SimpleUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
@@ -14,6 +16,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
@@ -22,8 +25,6 @@ public class SymphonyClient {
     @Value("${wdk.studio.admins:}")
     private List<Long> admins;
     private final UserService users;
-    record SimpleUser(Long id, String displayName) {}
-    record Profile(boolean isAdmin) {}
 
     @GetMapping("symphony/profile")
     public Profile getProfile(@RequestAttribute("user") UserClaim user) {
@@ -50,7 +51,7 @@ public class SymphonyClient {
             .sorted(Comparator.comparing(UserV2::getDisplayName))
             .limit(10)
             .map(u -> new SimpleUser(u.getId(), u.getDisplayName()))
-            .toList();
+            .collect(toList());
     }
 
     @PostMapping("symphony/users")
