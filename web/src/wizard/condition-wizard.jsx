863bd891ca16ba82076668c8a06a327e8f766689
Yong Sheng Tan <yong.tan@symphony.com>
2023-06-13 15:00:02 +0200

    Deprecation notice

--- web/src/wizard/condition-wizard.jsx
+++
@@ -1,9 +0,0 @@
-import { TextField } from "@symphony-ui/uitoolkit-components/components";
-
-const ConditionWizard = () => (
-    <>
-        Define the conditional execution of the activity
-        <TextField name="condition" label="Execute the activity only if" />
-    </>
-);
-export default ConditionWizard;
